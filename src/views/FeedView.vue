d486a89bae8e654e48974a109f7f678b65d15bbf
Øystein K. Repp <oysteinr@netcompany.com>
2020-05-10 20:45:50 +0200

    Solves assignment 3.

---
+++ src/views/FeedView.vue
@@ -0,0 +1,16 @@
+<template>
+  <list/>
+</template>
+
+<script>
+import List from '../components/feed/List.vue';
+
+export default {
+  name: 'FeedView',
+  components: { List },
+};
+</script>
+
+<style scoped>
+
+</style>
